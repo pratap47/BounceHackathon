3b960a73a8526bb15bb7765fcbb53daef528eafe
quoraboy <sachin.s.pathak1998@gmail.com>
2019-08-04 02:58:50 +0530

    some changes pushed

--- app/src/main/java/test/com/apnigaadi/Vehicle_list/WantCar.java
+++ app/src/main/java/test/com/apnigaadi/Vehicle_list/WantCar.java
@@ -1,9 +1,14 @@
 package test.com.apnigaadi.Vehicle_list;
 
-import android.content.Context;
-import android.net.Uri;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -16,7 +21,11 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,6 +41,11 @@ public class WantCar extends Fragment {
     private RecyclerView.Adapter adapter;
     private List<Listitem> listitems;
     private RequestQueue requestQueue ;
+    private int code = 101;
+    public static boolean mLocationPermissionGranted = false;
+    Intent i;
+    FusedLocationProviderClient fusedLocationProviderclient;
+    private Location currentlocation;
 
     public WantCar() {
         // Required empty public constructor
@@ -79,8 +93,167 @@ public class WantCar extends Fragment {
         });
         Mysingleton.getInstance(getActivity()).addToRequestqueue(ExampleRequest);
 
+    getLocationPermission();
+getDeviceLocation();
          return w;
+
     }
+
+
+
+
+
+
+
+
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(getContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED)
+        {
+            if (ContextCompat.checkSelfPermission(getContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+
+                //If the location permission has been granted, then start the TrackerService//
+//                i =  new Intent(getActivity(), TrackingService.class);
+//                getActivity().startService(i);
+
+
+            }
+            else {
+
+                //If the app doesn’t currently have access to the user’s location, then request access//
+
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        code);
+            }
+        }
+        else
+        {
+
+            //If the app doesn’t currently have access to the user’s location, then request access//
+
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    code);
+        }
+
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //Toast.makeText(this,"We are in onRequestPermission",Toast.LENGTH_SHORT).show();
+//If the permission has been granted...//
+
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case 101: {
+                if (grantResults.length > 0) {
+                    for (int j = 0; j < grantResults.length; j++) {
+                        if (grantResults[j] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionGranted = true;
+                            //...then start the GPS tracking service//
+
+                            Toast.makeText(getContext(), "onRequestPermissionResult", Toast.LENGTH_SHORT).show();
+//                            i =  new Intent(getActivity(), TrackingService.class);
+//                            getActivity().startService(i);
+
+                            return;
+                        }
+                        else
+                        {
+                            //If the user denies the permission request, then display a toast with some more information//
+
+                            Toast.makeText(getContext(), "Please enable location services to allow GPS tracking", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+
+
+//        mDatabase1 = FirebaseDatabase.getInstance().getReference();
+        LocationRequest request = new LocationRequest();
+        request.setInterval(10000);
+//        request.setFastestInterval(1000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        fusedLocationProviderclient = LocationServices.getFusedLocationProviderClient(getActivity());
+//        SharedPreferences sharedPreferences = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+//        uid=sharedPreferences.getString("Uid",Default);
+//        phoneno=sharedPreferences.getString("Phone",Default);     //Default value get displayed when no data is entered in main activity
+//        name=sharedPreferences.getString("name",Default);
+
+//        mDatabase1=mDatabase1.child(phoneno);
+//      map.alert();
+
+        try {
+            if (WantCar.mLocationPermissionGranted) {
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                fusedLocationProviderclient.requestLocationUpdates(request,
+                        new LocationCallback() {
+                            @Override
+                            public void onLocationResult(LocationResult locationresult) {
+//
+
+                                currentlocation= locationresult.getLastLocation();
+//                                speed=currentlocation.getSpeed()*18/5;
+
+//                        new DecimalFormat("#.##").format(speed)
+           Toast.makeText(getContext(),Double.toString(currentlocation.getLatitude()),Toast.LENGTH_LONG).show();
+                                Toast.makeText(getContext(),Double.toString(currentlocation.getLongitude()),Toast.LENGTH_LONG).show();
+//                                mDatabase1.child("Latitude").setValue(Double.toString(currentlocation.getLatitude()));
+//                                mDatabase1.child("Longitude").setValue(Double.toString(currentlocation.getLongitude()));
+
+
+                            }
+                        }, null);
+            }
+        }
+        catch (SecurityException e)
+        {
+            Toast.makeText(getContext(),"We are in getDeviceLocation>>catch",Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+
+
+
 
 
 }
